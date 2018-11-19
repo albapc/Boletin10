1f3f2ca8cd44b464e31f88704fc8e91e39cc9811
albapc <alba.perez.ce@gmail.com>
2018-11-19 16:53:59 +0100

    terminar

--- src/boletin10_1/Boletin10_1.java
+++ src/boletin10_1/Boletin10_1.java
@@ -1,14 +1,13 @@
-
 package boletin10_1;
 
 public class Boletin10_1 {
 
     public static void main(String[] args) {
-        
-        Xogo obxNumeros= new Xogo();
-        
+
+        Xogo obxNumeros = new Xogo();
+
         obxNumeros.introducirDatos();
         obxNumeros.averiguarNumeros();
     }
-    
+
 }
